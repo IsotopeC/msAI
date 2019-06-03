56ff6418b25eaa0092621ec0c34e445877d71ec7
Calvin <calvin.isotope@gmail.com>
2019-06-03 17:49:08 -0500

    Updated setup

--- setup.py
+++ setup.py
@@ -15,9 +15,16 @@ setuptools.setup(
     url="https://github.com/IsotopeC/msAI",
     packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3",
-                 "Operating System :: OS Independent",],
-    keywords='AI models mass spectrometry data',
-    install_requires=['numpy', 'pandas'],
+                 "Operating System :: OS Independent",
+                 "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                 "Topic :: Scientific/Engineering :: Bio-Informatics",
+                 "Topic :: Scientific/Engineering :: Chemistry",
+                 "Topic :: Scientific/Engineering :: Mathematics",
+                 "Topic :: Scientific/Engineering :: Visualization"],
+    keywords='AI models mass spectrometry MS data mzML lifecycle management training testing validation'
+             'quality analysis QA mass spectrum MS total ion chromatogram TIC extracted-ion chromatogram'
+             'EIC XIC machine learning artificial intelligence deep neural network',
+    install_requires=['numpy', 'pandas', 'matplotlib', 'tensorflow', 'bokeh', 'pymzml'],
     python_requires='>3.4'
 )
 
