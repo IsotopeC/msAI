c27691671ffb2149561143bc1ad1beee378e13b8
Calvin <calvin.isotope@gmail.com>
2019-11-25 11:59:30 -0600

    docstring reformats

--- msAI/errors.py
+++ msAI/errors.py
@@ -34,99 +34,69 @@ class RootError(msAIerror):
 
 
 class MiscUtilsError(msAIerror):
-    """
-    Exceptions raised for errors in miscUtils
+    """Exceptions raised for errors in miscUtils the module."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
+        """Initializes an instance of MiscUtilsError.
+
+        Args:
+            message: Explanation of the cause of this error.
+        """
+
         self.message = message
 
 
 class MetadataError(msAIerror):
-    """
-    Exceptions raised for errors in metadata
+    """Exceptions raised for errors in the metadata module."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class MetadataInitError(MetadataError):
-    """
-    Exceptions raised for errors in initializing metadata
+    """Exceptions raised for errors in initializing metadata."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class MetadataVerifyError(MetadataError):
-    """
-    Exceptions raised for errors in verifying imported metadata
+    """Exceptions raised for errors in verifying imported metadata."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class MetadataIndexError(MetadataError):
-    """
-    Exceptions raised for errors in setting metadata index
+    """Exceptions raised for errors in setting metadata index."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class SampleRunError(msAIerror):
-    """
-    Exceptions raised for errors in SampleRun
+    """Exceptions raised for errors in the SampleRun module."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class SampleRunMSinitError(SampleRunError):
-    """
-    Exceptions raised for errors in initializing MS data in SampleRun
+    """Exceptions raised for errors in initializing MS data in SampleRun."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class MSdataError(msAIerror):
-    """
-    Exceptions raised for errors in msData
+    """Exceptions raised for errors in the msData module."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
 
 
 class MSfileSetInitError(MSdataError):
-    """
-    Exceptions raised for errors in initializing MSfileSet
+    """Exceptions raised for errors in initializing MSfileSet."""
 
-    Attributes:
-        message -- explanation of the error
-    """
     def __init__(self, message):
         self.message = message
